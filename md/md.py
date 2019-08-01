27c1964892a235156f6043adb129aaf271207589
H. Watanabe <kaityo@users.sourceforge.jp>
2019-08-01 19:47:50 +0900

    get_atoms

--- md/md.py
+++ md/md.py
@@ -1,14 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
 
-myfont = ImageFont.truetype("fonts/ipaexg.ttf", 24)
-img = Image.new('1', (100, 100), 'white')
-draw = ImageDraw.Draw(img)
-draw.text((10, 10), "日本語", font=myfont)
 
-w, h = img.size
+def get_atoms(text):
+    myfont = ImageFont.truetype("fonts/ipaexg.ttf", 24)
+    img = Image.new('1', (100, 100), 'white')
+    draw = ImageDraw.Draw(img)
+    draw.text((10, 10), text, font=myfont)
 
-for x in range(w):
-    for y in range(h):
-        v = img.getpixel((x,y))
-        if v is 0:
-            print('{} {}'.format(x,y))
+    w, h = img.size
+
+    for x in range(w):
+        for y in range(h):
+            v = img.getpixel((x, y))
+            if v is 0:
+                print('{} {}'.format(x, y))
+
+
+get_atoms("日本語")
