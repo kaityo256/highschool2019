59a6caf3d54d80973b70e8e8db753114a86445f0
H. Watanabe <kaityo@users.sourceforge.jp>
2019-08-01 21:03:36 +0900

    bonds to qx, qy

--- md/md.py
+++ md/md.py
@@ -8,39 +8,41 @@ def get_atoms(text):
     draw.text((10, 10), text, font=myfont)
 
     w, h = img.size
-    atoms = []
+    qx = []
+    qy = []
     for x in range(w):
         for y in range(h):
             v = img.getpixel((x, y))
             if v is 0:
-                atoms.append([float(x), h - float(y)])
-    return atoms
+                qx.append(float(x))
+                qy.append(h-float(y))
+    return qx, qy
 
 
-def get_bonds(atoms):
-    n = len(atoms)
+def get_bonds(qx, qy):
+    n = len(qx)
     bonds = []
     for i in range(n-1):
-        (xi, yi) = atoms[i]
+        (xi, yi) = qx[i], qy[i]
         for j in range(i + 1, n):
-            (xj, yj) = atoms[j]
+            (xj, yj) = qx[j], qy[j]
             r2 = (xi - xj) ** 2 + (yi - yj) ** 2
             if r2 < 3.1:
                 bonds.append((i, j, r2))
     return bonds
 
 
-def show_bonds(atoms, bonds):
+def show_bonds(qx, qy, bonds):
     for i, j, _ in bonds:
-        (xi, yi) = atoms[i]
-        (xj, yj) = atoms[j]
+        (xi, yi) = qx[i], qy[i]
+        (xj, yj) = qx[j], qy[j]
         print(xi, yi, (xj-xi), (yj-yi))
 
 
 def main():
-    atoms = get_atoms("スパコン")
-    bonds = get_bonds(atoms)
-    show_bonds(atoms, bonds)
+    qx, qy = get_atoms("スパコン")
+    bonds = get_bonds(qx, qy)
+    show_bonds(qx, qy, bonds)
    # for x, y in atoms:
     #    print('{} {}'.format(x, y))
 
