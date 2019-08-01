2d46a9524ce0440b4d5182cc504dbe0eb4300677
H. Watanabe <kaityo@users.sourceforge.jp>
2019-08-01 20:03:31 +0900

    bonds

--- md/md.py
+++ md/md.py
@@ -8,12 +8,37 @@ def get_atoms(text):
     draw.text((10, 10), text, font=myfont)
 
     w, h = img.size
-
+    atoms = []
     for x in range(w):
         for y in range(h):
             v = img.getpixel((x, y))
             if v is 0:
-                print('{} {}'.format(x, y))
+                atoms.append([float(x), float(y)])
+    return atoms
 
 
-get_atoms("日本語")
+def get_bonds(atoms):
+    n = len(atoms)
+    bonds = []
+    for i in range(n-1):
+        (xi, yi) = atoms[i]
+        for j in range(i + 1, n):
+            (xj, yj) = atoms[j]
+            r2 = (xi - xj) ** 2 + (yi - yj) ** 2
+            if r2 < 3.1:
+                bonds.append((i, j, r2))
+    return bonds
+
+
+def main():
+    atoms = get_atoms("日本語")
+    bonds = get_bonds(atoms)
+    for i, j, _ in bonds:
+        (xi, yi) = atoms[i]
+        (xj, yj) = atoms[j]
+        print(xi, yi, (xj-xi), (yj-yi))
+    # for x, y in atoms:
+    #    print('{} {}'.format(x, y))
+
+
+main()
